e970c56ec5261444ef48a253635e290c13c2a26d
NotMyWing <winwyv@gmail.com>
2021-04-04 00:13:49 +1100

    Make CI prune cache

--- buildtools/gulpfile.ts
+++ buildtools/gulpfile.ts
@@ -8,10 +8,11 @@ import clientTasks from "./tasks/client";
 import serverTasks from "./tasks/server";
 import langTasks from "./tasks/lang";
 
-export const buildClient = gulp.series(sharedTasks, clientTasks, pruneCacheTask);
-export const buildServer = gulp.series(sharedTasks, serverTasks, pruneCacheTask);
+export const buildClient = gulp.series(sharedTasks, clientTasks);
+export const buildServer = gulp.series(sharedTasks, serverTasks);
 export const buildLang = gulp.series(sharedTasks, langTasks);
-export const buildAll = gulp.series(sharedTasks, gulp.series(clientTasks, serverTasks, langTasks, pruneCacheTask));
+export const buildAll = gulp.series(sharedTasks, gulp.series(clientTasks, serverTasks, langTasks));
+export const buildAllAndPrune = gulp.series(buildAll, pruneCacheTask);
 
 import checkTasks from "./tasks/checks";
 export const check = gulp.series(checkTasks);
